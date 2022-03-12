f0ef2bf235388f4c568e71d4c73c135c2173f437
hugosenari <hugosenari@gmail.com>
2022-03-12 01:36:47 -0300

    chore: re index zlib

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''zlib wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."zlib-master".url = "path:./master";
+  inputs."zlib-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zlib-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''zlib wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-zlib-master.flake = false;
+  inputs.src-zlib-master.owner = "status-im";
+  inputs.src-zlib-master.ref   = "refs/heads/master";
+  inputs.src-zlib-master.repo  = "nim-zlib";
+  inputs.src-zlib-master.type  = "github";
+  
+  inputs."stew".dir   = "nimpkgs/s/stew";
+  inputs."stew".owner = "riinr";
+  inputs."stew".ref   = "flake-pinning";
+  inputs."stew".repo  = "flake-nimble";
+  inputs."stew".type  = "github";
+  inputs."stew".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stew".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zlib-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-zlib-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
